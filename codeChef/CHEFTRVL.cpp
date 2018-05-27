17ae824d8a462d9c398a56683fc5152a0460f91e
ay2306 <mahajan.ayush2306@gmail.com>
2018-05-28 04:02:52 +0530

    CHEFTRVL

--- codeChef/CHEFTRVL.cpp
+++ codeChef/CHEFTRVL.cpp
@@ -11,51 +11,107 @@ class graph{
 public:
     graph(int vertices);
     void addEdge(int source, int destination);
+    bool BFS(int start, int dest, int pred[], int dist[]);
+    void printList(list<int> li);
+    int  shortestPath(int start, int destination);
+    void DFS(int start);
+    void DFSstep(int node, bool *visited,int level);
     void enterAges();
     int getAnswer();
-    int findDistance(int a, int b);
-    int dist(int node, int dest, bool *visited, int distance);
-
     // void BFSstep(int node, bool *visited, int level)
 };
 
 graph::graph(int vertices){
     this->vertices = vertices;
     edges = new list<int>[vertices+1];
-    age = new int[vertices];
+    age = new int[vertices + 1];
 }
 
 void graph::addEdge(int source, int destination){
     edges[source].push_back(destination);
     edges[destination].push_back(source);
 }
- 
-void graph::enterAges(){
-    for(int i = 1; i <= vertices; ++i){
-        cin >> age[i];
-    }
-}
 
-int graph::dist(int node, int dest, bool *visited, int distance){
-    if(node == dest)return distance;
-    for(list<int>::iterator itr = edges[node].begin(); itr!=edges[node].end(); ++itr){
-        if(!visited[*itr]){
-            visited[*itr] = true;
-            return (1 + dist(*itr,dest,visited,1+distance));
+bool graph::BFS(int start, int dest, int pred[], int dist[]){
+    bool *visited = new bool[vertices+1];
+    for(int i = 1; i <= vertices; ++i){
+        visited[i] = false;
+        dist[i] = INT_MAX;
+        pred[i] = -1;
+    }
+    list<int> q;    
+    q.push_back(start);
+    visited[start] = true;
+    dist[start] = 0;
+    // cout << start << "  ";
+    while(!q.empty()){
+        int s = q.front();
+        // cout << s << "  ";
+        // printList(q);
+        q.pop_front();
+        for(list<int>::iterator i = edges[s].begin(); i != edges[s].end(); ++i){
+            if(!visited[*i]){
+                visited[*i] = true;
+                dist[*i] = dist[s] + 1;
+                pred[*i] = s;
+                q.push_back(*i);
+                if(*i == dest)return true;
+            }
         }
     }
-    return ((-1* distance)-200);    
+    // BFSstep(start,visited,0);
+    return false;
+} 
+
+void graph::DFS(int start){
+    bool *visited = new bool[vertices+1];
+    for(int i = 0; i <= vertices; ++i){
+        visited[i] = false;
+    } 
+    int level = 0;
+    visited[start] = true;
+    cout << start << " ";
+    for(list<int>::iterator itr = edges[start].begin(); itr != edges[start].end(); ++itr){
+        DFSstep(*itr, visited, level+1);
+    }
 }
 
-int graph::findDistance(int a, int b){
-    bool *visited = new bool[vertices + 1];
-    for(int i = 1; i < vertices; ++i)visited[i]=false;
-    visited[a] = true;
-    list<int>::iterator itr = edges[a].begin();
-    int n = dist(*itr,b,visited,1);
-    ++itr;
-    int m = dist(*itr,b,visited,1);
-    return (n>m)?n:m;
+void graph::DFSstep(int node, bool *visited, int level){
+    visited[node] = true;
+    cout << node << " ";
+    for(list<int>::iterator itr = edges[node].begin(); itr != edges[node].end(); ++itr){
+        if(!visited[*itr]){
+            DFSstep(*itr, visited, level+1);
+        }
+    }
+}
+int graph::shortestPath(int start, int destination){
+    int *pred = new int[vertices + 1];
+    int *dist = new int[vertices + 1];
+    if(!BFS(start,destination,pred,dist)){
+        // cout << "PATH NOT CONNECTED";
+        return -1;
+    }
+    int crawl = destination;
+    list<int> path;
+    path.push_front(crawl);
+    while(pred[crawl] != -1){
+        path.push_front(pred[crawl]);
+        crawl = pred[crawl];
+    }
+    // for(list<int>::iterator itr  = path.begin(); itr != path.end(); ++itr){
+    //     cout << *itr << "\t";
+    // }
+    // cout << "\nLength of shortest Path = " << (path.size()-1) << endl;
+    return path.size()-1;
+}
+
+void graph::printList(list<int> li){
+    cout << "LIST: ";
+    for(list<int>::iterator itr = li.begin(); itr != li.end(); ++itr){
+        cout << *itr << "\t";
+    }
+    cout << endl;
 }
 
 int graph::getAnswer(){
@@ -68,13 +124,20 @@ int graph::getAnswer(){
                 if(k%2 == 1)bits++;
                 k = (k>>1);
             }
-            if(findDistance(i,j)%2 != bits%2){
+            if(shortestPath(i,j)%2 != bits%2){
                 ans++;
             }
         }
     }
     return ans;
 }
+
+void graph::enterAges(){
+    for(int i = 1; i <= vertices; ++i){
+        cin >> age[i];
+    }
+}
+
 
 int main(){
     int t;
